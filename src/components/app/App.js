2772c8da9452dc0cae19cdedd63b5a6f28107b3d
Egor Dultsev <listenmy@yandex.ru>
2022-04-19 20:49:57 +0300

    Initial commit

---
+++ src/components/app/App.js
@@ -0,0 +1,46 @@
+import {lazy, Suspense} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/Spinner';
+import SinglePage from "../pages/SinglePage";
+import SingleComicLayout from "../pages/singleComicLayout/SingleComicLayout";
+import SingleCharacterLayout from "../pages/singleCharacterLayout/SingleCharacterLayout";
+
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+
+const App = () => {
+    
+    return (
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Suspense fallback={<Spinner/>}>
+                        <Switch>
+                            <Route exact path="/">
+                                <MainPage/>
+                            </Route>
+                            <Route exact path="/comics">
+                                <ComicsPage/>
+                            </Route>
+                            <Route exact path="/comics/:id">
+                                <SinglePage Component={SingleComicLayout} dataType="comic"/>
+                            </Route>
+                            <Route exact path="/characters/:id">
+                                <SinglePage Component={SingleCharacterLayout} dataType="character"/>
+                            </Route>
+                            <Route path="*">
+                                <Page404/>
+                            </Route>
+                        </Switch>
+                    </Suspense>
+                </main>
+            </div>
+        </Router>
+    )
+}
+
+export default App;
